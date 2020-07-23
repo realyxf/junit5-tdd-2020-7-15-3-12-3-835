c3addc3c8a297e4f6c1aa5bbded6587f6eebc97c
realyxf <997915421@qq.com>
2020-07-23 11:37:36 +0800

    [Fennie] test:should_return_1_when_count_off_given_1

---
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_1() {
+        //Given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int inputNumber = 1;
+
+        //When
+        String result = fizzBuzz.play(inputNumber);
+
+        //Then
+        assertEquals("1",result);
+    }
+}
