53b4f1cff18b405e2593443e6ab194cc86c6decd
realyxf <997915421@qq.com>
2020-07-23 11:53:35 +0800

    [Fennie] test: should_return_fizzbuzz_when_count_off_given_15

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -43,4 +43,17 @@ public class FizzBuzzTest {
         //Then
         assertEquals("Buzz",result);
     }
+
+    @Test
+    void should_return_fizzbuzz_when_count_off_given_15() {
+        //Given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int inputNumber = 15;
+
+        //When
+        String result = fizzBuzz.play(inputNumber);
+
+        //Then
+        assertEquals("FizzBuzz",result);
+    }
 }
